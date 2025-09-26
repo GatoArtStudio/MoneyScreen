424bb2017c7307133aea9232772b4b654c5faa0c
GatoArtStudio <hackhcctecno@gmail.com>
2025-09-26 12:58:22 -0500

    Refactored: Reduces the amount of duplicate code and increases readability

--- src/main/java/art/gatoartstudio/moneyscreen/server/command/ShowViewCommand.java
+++ src/main/java/art/gatoartstudio/moneyscreen/server/command/ShowViewCommand.java
@@ -5,6 +5,7 @@ import art.gatoartstudio.moneyscreen.common.payload.MoneyUpdatePayload;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -16,28 +17,33 @@ public class ShowViewCommand {
                 .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
                 .then(CommandManager.argument("money", IntegerArgumentType.integer())
                         .then(CommandManager.argument("players", IntegerArgumentType.integer())
-                                .executes(context -> {
-                                    int money = IntegerArgumentType.getInteger(context, "money");
-                                    int players = IntegerArgumentType.getInteger(context, "players");
-
-                                    // Here you would implement the logic to show the money screen to players
-                                    context.getSource().sendFeedback(() -> Text.literal("MoneyScreen command executed, datos recibidos: money=" + money + ", players=" + players), false);
-
-                                    MoneyUpdatePayload moneyUpdatePayload = new MoneyUpdatePayload(money, players);
-                                    context.getSource().getServer().getPlayerManager().getPlayerList()
-                                            .forEach(player -> {
-                                                ServerPlayNetworking.send(player, moneyUpdatePayload);
-                                                Moneyscreen.LOGGER.info("Sent MoneyUpdatePayload to player {}: money={}, players={}", player.getName().getString(), money, players);
-                                            });
-
-                                    return Command.SINGLE_SUCCESS;
-                                })
+                                .executes(ShowViewCommand::execute)
                         )
+                        .executes(ShowViewCommand::executeHelp)
                 )
-                .executes(context -> {
-                    context.getSource().sendFeedback(() -> Text.literal("Please user: /moneyscreen money players, example: /moneyscreen 120 520"), false);
-                    return Command.SINGLE_SUCCESS;
-                })
+                .executes(ShowViewCommand::executeHelp)
         );
+    }
+
+    public static int execute(CommandContext<ServerCommandSource> context) {
+        int money = IntegerArgumentType.getInteger(context, "money");
+        int players = IntegerArgumentType.getInteger(context, "players");
+
+        // Here you would implement the logic to show the money screen to players
+        context.getSource().sendFeedback(() -> Text.literal("MoneyScreen command executed, datos recibidos: money=" + money + ", players=" + players), false);
+
+        MoneyUpdatePayload moneyUpdatePayload = new MoneyUpdatePayload(money, players);
+        context.getSource().getServer().getPlayerManager().getPlayerList()
+                .forEach(player -> {
+                    ServerPlayNetworking.send(player, moneyUpdatePayload);
+                    Moneyscreen.LOGGER.info("Sent MoneyUpdatePayload to player {}: money={}, players={}", player.getName().getString(), money, players);
+                });
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    public static int executeHelp(CommandContext<ServerCommandSource> context) {
+        context.getSource().sendFeedback(() -> Text.literal("Please user: /moneyscreen money players, example: /moneyscreen 120 520"), false);
+        return Command.SINGLE_SUCCESS;
     }
 }
